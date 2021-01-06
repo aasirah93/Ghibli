d60023e69661315c9040d637c75245c919756eb1
Asifur Rahman <aasirah93@hotmail.co.uk>
2021-01-06 23:57:30 +0000

    fetch example

--- ghibli/src/App.js
+++ ghibli/src/App.js
@@ -22,4 +22,8 @@ function App() {
   );
 }
 
+fetch('https://api.github.com/users/hacktivist123/repos')
+  .then(response => response.json())
+  .then(data => console.log(data));
+
 export default App;
