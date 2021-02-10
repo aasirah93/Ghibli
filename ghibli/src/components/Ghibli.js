c4db5049d004b3d3c8a264187b1762a5dddbfce9
Asifur Rahman <aasirah93@hotmail.co.uk>
2021-02-10 23:57:24 +0000

    Sidebar

--- ghibli/src/components/Ghibli.js
+++ ghibli/src/components/Ghibli.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
+// const[fetch] = fetch('https://ghibliapi.herokuapp.com/films')
 
 function Ghibli() {
     const [items, setItems] = useState([]);
---
+++ ghibli/src/components/Sidebar.js
@@ -0,0 +1,11 @@
+import "./Sidebar.css";
+import React from "react";
+
+export const Sidebar = ({ width, height children }) => {
+
+  return (
+      <div className="side-bar" style={{width: width, minHeight: height }}>
+        <React.Fragment>{children}<React.Fragment/>
+      </div>
+  );
+};
