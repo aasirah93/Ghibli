1dd3cfd2c130e0737961848c6ba564b933406061
Asifur Rahman <aasirah93@hotmail.co.uk>
2021-03-01 23:56:32 +0000

    Update useState

--- ghibli/src/components/Searchbar.js
+++ ghibli/src/components/Searchbar.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 
 function Searchbar(){
 
-  const [search, setSearch] = React.useState("");
+  const [search, setSearch] = useState("");
 
 return (
 
